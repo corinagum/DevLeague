175e905e211b403ab5ea38a81e7ec3f3f9d217b7
Corina <corina.aj@gmail.com>
2015-12-02 16:38:24 -1000

    I don't know

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,57 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
-  // do your work here
+  var i = 2;
+  while(i<= n) {
+    if(isDivisible(n,i)) {
+
+      if(isPrime(i)) {
+
+      console.log(i);
+      primeNumber = i;
+     }
+    }
+    i++;
+  }
 
   return primeNumber;
 };
+
+
+function isPrime(value) {
+  for (var i = 2; i < value; i++) {
+    if(value%i === 0) {
+      return false;
+    }
+    else {
+      return value > 1;
+    }
+  }
+}
+
+function isDivisible(n,divider) {
+  if(n % divider === 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+// function largestPrime(n) {
+//   var i = 2;
+//   while(i <= n) { //why does it have to be less than OR EQUAL TO?
+//     if(n%i === 0) {
+//       n /= i; //??
+//       if(isPrime(i)) {
+//         primeNumber = i;
+//       }
+//     }
+//     else {
+//       i++;
+//     }
+//   }
+//   return primeNumber;
+// }
+
+// primeNumber = largestPrime(n);
