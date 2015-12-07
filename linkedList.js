700c2c04dc8b1a0a7e3541001f6fbd2d3a30c3f8
Corina <corina.aj@gmail.com>
2015-12-07 13:40:14 -1000

    getTail function passing; add function not

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,36 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  var methods = {};
 
+  methods.getHead = function(){
+    if(this.value === undefined) {
+      return null;
+    }
+      return this.value;
+  };
+  methods.getTail = function(){
+    if(this.value === undefined) {
+      return null;
+    }
+    else if(this.value!==null) {
+      return this.next;
+    }
+    return this.value.getTail();
+  };
+  methods.add = function(){
+    this.getTail().next === {
+      value: null,
+      next: null
+    };
+
+  };
+  methods.remove = function(){};
+  methods.get = function(){};
+  methods.insert = function(){};
+
+  return methods;
 }
+
+var linkedList = linkedListGenerator();
+linkedList.getHead();
