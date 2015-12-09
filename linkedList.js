ea719b42c733a1d247a2f0902239d6813692cb49
Corina <corina.aj@gmail.com>
2015-12-08 15:02:49 -1000

    finish - had to reconstruct get add and remove in order to get insert to work - needed to use variable length

--- linkedList.js
+++ linkedList.js
@@ -7,6 +7,14 @@ function linkedListGenerator(){
   var methods = {};
   var head = null;
   var tail = null;
+  var length = 0;
+
+ function createNode (value) {
+    return {
+      value: value,
+      next: null
+    };
+  }
 
   methods.getHead = function(){
     return head;
@@ -17,74 +25,57 @@ function linkedListGenerator(){
   };
 
   methods.add = function(value){
-    var newNode = {
-      value: value,
-      next: null
-    };
+    var newNode = createNode(value);
 
-    if(head === null && tail === null) {
+    if(tail === null) {
       head = newNode;
-      tail = newNode;
-      return newNode;
     } else {
       tail.next = newNode;
-      tail = newNode;
-      return tail; // do we need to fix this?
     }
+      tail = newNode;
+      length++;
+      return newNode; // do we need to fix this?
   };
 
-  methods.get = function(number){
+  methods.get = function(index){
+    var currentNode = this.getHead();
     var i = 0;
-    var currentNode = head;
-    if(i === number) {
-      return currentNode;
-    } else {
-      while(i !== number) {
-        if(currentNode.next === null && i!== number) {
-          return false;
-        } else { currentNode = currentNode.next;
-          i++;
-        }
-      }
-      return currentNode;
-    }
-  };
-  methods.insert = function(value, index){
-    var i = 0;
-    var currentNode = head;
-    var nextNode = currentNode.next;
-    var nextHolder;
-    if(index === 0) {
-      nextHolder = head;
-      head.value = value;
-      head.next = nextHolder;
-    }
-    while (i !== index) {
-      // currentNode = nextNode;
-    // console.log('hello', i, index, currentNode.next, nextNode.next);
-    // console.log('head', head);
-      if(currentNode.next === null) {
+
+    while (i < index) {
+      if(currentNode === tail) {
         return false;
-      } else {
-        currentNode = currentNode.next;
-      console.log(currentNode, nextNode);
       }
-      currentNode = nextNode;
-      currentNode = value;
-      currentNode.next = nextNode;
+      currentNode = currentNode.next;
       i++;
     }
-// console.log(currentNode);
-// if i = index, invoke add
-    // nextHolder = currentNode;
-    // currentNode.value = value;
-    // currentNode.next = nextHolder.value;
+    return currentNode;
+  };
+
+  methods.insert = function(value, index){
+
+    var currentNode = this.get(index);
+    var previousNode = this.get(index - 1);
+    var tempNode = createNode(value);
+
+
+    if( index >= length || index < 0) {
+      return false;
+    } else if(index === 0) {
+      // tempNode = createNode(value);
+      tempNode.next = currentNode;
+      head = tempNode;
+
+    } else {
+      // tempNode = createNode(value);
+      previousNode.next = tempNode;
+      tempNode.next = currentNode;
+      }
+    length++;
   };
   methods.remove = function(number){
     var currentNode = head;
     var nextNode = currentNode.next;
     var i = 0;
-    var previousNode = currentNode;
 
     if(number === 0) {
       head = currentNode.next;
@@ -103,9 +94,12 @@ function linkedListGenerator(){
         }
         nextNode = nextNode.next;
       i++;
+      length --;
       }
     }
 
   };
   return methods;
 }
+
+// Joe's version https://gist.github.com/JoeKarlsson1/5e42597528876e40e208
