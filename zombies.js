66dcbe8bf9f2f7baa0ccd374ff7150cfa95b2f24
Corina <corina.aj@gmail.com>
2015-12-09 18:52:09 -1000

    item class pass

--- zombies.js
+++ zombies.js
@@ -7,7 +7,13 @@
  * @param {string} name     The item's name.
  * @property {string} name
  */
-
+function Item(name) {
+  if(typeof (name) === 'string') {
+    this.name = name;
+  } else {
+    throw new TypeError('Item should be a string');
+  }
+}
 
 /**
  * Class => Weapon(name, damage)
@@ -15,7 +21,7 @@
  * Creates a weapon item.
  * Weapon items can be equipped for use in battle.
  *
- * The Weapon class constructor will call 
+ * The Weapon class constructor will call
  *   the super class (Item) constructor
  *   while passing in the 1 Item constructor param
  *
@@ -24,7 +30,11 @@
  * @param {number} damage   The weapon's damage.
  * @property {number} damage
  */
+Weapon.prototype = Object.create(Item.prototype);
 
+function Weapon(name, damage) {
+
+}
 
 /**
  * Weapon Extends Item Class
@@ -39,7 +49,7 @@
  * Creates a food item.
  * Food items give energy, restoring health to the player.
  *
- * The Food class constructor will call 
+ * The Food class constructor will call
  *   the super class (Item) constructor
  *   while passing in the 1 Item constructor param
  *
@@ -231,7 +241,7 @@
  * -----------------------------
  * Creates a fast zombie.
  *
- * The FastZombie class constructor will call 
+ * The FastZombie class constructor will call
  *   the super class (Zombie) constructor
  *   while passing in the 3 Zombie constructor params
  *
@@ -254,7 +264,7 @@
  * -----------------------------
  * Creates a strong zombie.
  *
- * The StrongZombie class constructor will call 
+ * The StrongZombie class constructor will call
  *   the super class (Zombie) constructor
  *   while passing in the 3 Zombie constructor params
  *
@@ -277,7 +287,7 @@
  * -----------------------------
  * Creates a ranged zombie.
  *
- * The RangedZombie class constructor will call 
+ * The RangedZombie class constructor will call
  *   the super class (Zombie) constructor
  *   while passing in the 3 Zombie constructor params
  *
@@ -300,7 +310,7 @@
  * -----------------------------
  * Creates an exploding zombie.
  *
- * The ExplodingZombie class constructor will call 
+ * The ExplodingZombie class constructor will call
  *   the super class (Zombie) constructor
  *   while passing in the 3 Zombie constructor params
  *
