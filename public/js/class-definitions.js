81e3e856639bfb0f8bb50090cdef2f62c46f5a37
Corina <corina.aj@gmail.com>
2015-12-15 21:00:04 -1000

    containsJunkFood method passing

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1325,7 +1325,16 @@ Cookie.prototype.swipedByCookieMonster = function (dayOfTheWeek) {
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
  *
  */
-
+var junkFood = ["chips", "soda", "ice cream", "popcorn", "candy"];
+Meal.prototype.containsJunkFood = function (foods) {
+  for (var i = 0; i < junkFood.length; i++) {
+    if (this.foods.indexOf(junkFood[i]) !== -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
 
  /* Steps 91 to 100
  *
