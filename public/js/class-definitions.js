93457a4265c584eb6bc21134dc81690f2b72ed3d
Corina <corina.aj@gmail.com>
2015-12-15 20:55:25 -1000

    swipedByCookieMonster method passing

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1304,6 +1304,13 @@ Lightbulb.prototype.flipSwitch = function (on) {
  *
  */
 
+Cookie.prototype.swipedByCookieMonster = function (dayOfTheWeek) {
+  if (this.flavor === "chocolate" && dayOfTheWeek === "Monday") {
+    return true;
+  } else {
+    return false;
+  }
+};
 
  /* Step 90
  *
