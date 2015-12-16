3c2335923eed891c02c4dcb8352ad9e8d501e492
Corina <corina.aj@gmail.com>
2015-12-15 15:36:49 -1000

    Beers passing

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -290,6 +290,14 @@ var scmList = [
  *
  */
 
+var beers = {
+  IPA: "Ale",
+  Lager: "Strong",
+  Heffeweisen: "German",
+  Stout: ["Thick", "Dark"],
+  Porter: "Bitter",
+  Ale: ["Light", "Golden"]
+};
 
 /* Step 20
  *
