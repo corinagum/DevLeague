21eb4270c7c61c7fefcc75993078e5983c538529
Corina <corina.aj@gmail.com>
2015-12-15 21:27:09 -1000

    Step 98 is passing

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1422,8 +1422,8 @@ var shortStory = new House(2).isATallStory(3);
  * and assign the values to each variable below.
  *
  */
-var kitchenLightsOn;
-var porchLightsOff;
+var kitchenLightsOn = new Lightbulb(false).flipSwitch("on");
+var porchLightsOff = new Lightbulb(true).flipSwitch("off");
 
 
  /* Step 99
