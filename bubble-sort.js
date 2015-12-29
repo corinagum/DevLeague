a02ffd511e35d8e4bdd596d5cbc6488f98b5191f
Corina <corina.aj@gmail.com>
2015-12-28 14:46:13 -1000

    establishes working prototype to create bubbleSort method

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,3 +1,30 @@
-module.exports = function bubbleSort (array) {
+function bubbleSort (unsortedArray) {
+  var switching = false;
 
-};
+  while(!switching) {
+    switching = true;
+
+    for ( var i = 0; i < this.length; i++ ) {
+
+      if(this[i] > this[i+1]) {
+        var bigger = this[i];
+        var smaller = this[i+1];
+
+        this[i] = smaller;
+        this[i+1] = bigger;
+
+        switching = false;
+      }
+    }
+  }
+  return this;
+}
+
+Array.prototype.bubbleSort = bubbleSort;
+
+console.log([2,1,3].bubbleSort());
+
+
+
+
+module.exports = bubbleSort;
--- test/bubble-sort.spec.js
+++ test/bubble-sort.spec.js
@@ -9,4 +9,10 @@ describe ('bubbleSort', function () {
     expect(bubbleSort).to.exist;
     expect(bubbleSort).to.be.a('function');
   });
+
+  it('should deep equal array', function() {
+    expect(bubbleSort([3,2,1])).to.deep.equal([1,2,3]);
+    expect(bubbleSort([3,2,1,4,12,8,5])).to.deep.equal([1, 2, 3, 4, 5, 8, 12]);
+
+  });
 });
