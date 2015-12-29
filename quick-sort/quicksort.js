c5afec0e8cdce9597eb0f957db0accfaa734d53d
Corina <corina.aj@gmail.com>
2015-12-28 18:59:02 -1000

    fixes module to only need 1 function

--- quick-sort/quicksort.js
+++ quick-sort/quicksort.js
@@ -1,14 +1,12 @@
-function quickSort (unsortedArray, a, b) {
-  var pivot = [];
+function quickSort (unsortedArray) {
   var left = [];
   var right = [];
-  // debugger;
+
   // Step 1: Pick an element, called a pivot, from the array
-  pivot = unsortedArray.splice(0, 1);
 
   // Step 2: Reorder the array
-  for (var i = 0; i < unsortedArray.length; i++) {
-    if (unsortedArray[i] <= pivot[0]) {
+  for (var i = 1; i < unsortedArray.length; i++) {
+    if (unsortedArray[i] <= unsortedArray[0]) {
       left.push(unsortedArray[i]);
     }
     else {
@@ -17,26 +15,15 @@ function quickSort (unsortedArray, a, b) {
   }
 
   //Step 3: Recursively apply the steps to the sub-array of elements
-  if (left.length <= 1 && right.length <= 1) {
-    var sortedArray = left.concat(pivot);
-    sortedArray = sortedArray.concat(right);
-    console.log("sorted array: " + sortedArray);
-    return sortedArray;
+  if (unsortedArray.length <= 1) {
+    return unsortedArray;
   }
   else {
-    quickSort(left, right);
+    return quickSort(left).concat(unsortedArray[0], quickSort(right));
   }
-  // else if (left.length > 1) {
-  //   console.log("left.length", left);
-  //   return quickSort(left);
-  // }
-
-  // else if (right.length > 1) {
-  //   console.log("right.length", right);
-  //   return quickSort(right);
-  // }
 }
 
 module.exports = quickSort;
-// quickSort([3,2,1,4]);
-quickSort([4,6,1,8,5]);
+// console.log(quickSort([3,2,1,4]));
+// console.log(quickSort([4,6,1,8,5,19,7]));
+// console.log(quickSort([1,3,2,4]));
