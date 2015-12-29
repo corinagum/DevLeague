bb774b12d188870bf1361d0d739357f1fcff3d8d
Corina <corina.aj@gmail.com>
2015-12-28 18:25:24 -1000

    step 3 not working

--- quick-sort/quicksort.js
+++ quick-sort/quicksort.js
@@ -1,9 +1,8 @@
-
-function quickSort (unsortedArray) {
-var pivot = [];
-var left = [];
-var right = [];
-  debugger;
+function quickSort (unsortedArray, a, b) {
+  var pivot = [];
+  var left = [];
+  var right = [];
+  // debugger;
   // Step 1: Pick an element, called a pivot, from the array
   pivot = unsortedArray.splice(0, 1);
 
@@ -19,21 +18,25 @@ var right = [];
 
   //Step 3: Recursively apply the steps to the sub-array of elements
   if (left.length <= 1 && right.length <= 1) {
-    var sortedArray = left.concat(pivot, right);
+    var sortedArray = left.concat(pivot);
+    sortedArray = sortedArray.concat(right);
     console.log("sorted array: " + sortedArray);
     return sortedArray;
   }
-
-  else if (left.length > 1) {
-    console.log("left.length", left.length);
-    quickSort(left);
+  else {
+    quickSort(left, right);
   }
+  // else if (left.length > 1) {
+  //   console.log("left.length", left);
+  //   return quickSort(left);
+  // }
 
-  else if (right.length > 1) {
-    console.log("right.length", right.length);
-    quickSort(right);
-  }
+  // else if (right.length > 1) {
+  //   console.log("right.length", right);
+  //   return quickSort(right);
+  // }
 }
 
 module.exports = quickSort;
-quickSort([3,2,1,4]);
+// quickSort([3,2,1,4]);
+quickSort([4,6,1,8,5]);
