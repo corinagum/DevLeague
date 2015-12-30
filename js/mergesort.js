c1e0de98d193232eb633aab3f13f6c901a48781c
Corina <corina.aj@gmail.com>
2015-12-29 18:03:53 -1000

    establishes mergesort as a module and connects it to the test file

---
+++ js/mergesort.js
@@ -0,0 +1,15 @@
+var mergeModule = module.exports = (function () {
+  return {
+    mergeSort: function (array) {
+
+
+      return array;
+    }
+  };
+});
+
+
+// if array is longer than 0 or 1, return it - it is already sorted
+// split array into roughly 1/2
+
+// continue to split until first index is equal to last index
---
+++ test/mergesort.spec.js
@@ -0,0 +1,5 @@
+var chai = require('chai');
+var mocha = require('mocha');
+chai.should();
+var expect = chai.expect;
+var mergeModule = require('./../js/mergesort.js');
