34c5b50971dc6094b8bbf35a96f1a4f1abd9a419
Corina <corina.aj@gmail.com>
2015-12-29 15:32:08 -1000

    establishes selectionsort module and tests if it is a function

--- js/selectionsort.js
+++ js/selectionsort.js
@@ -1,11 +1,24 @@
-'use strict';
-
+// 'use strict';
 var selectionModule = module.exports = (function () {
   return {
-
     selectionSort: function (array) {
-      //do stuff here
-      return 'HAVE NOT DONE YET';
+      for (var i = 0; i < array.length; i++) {
+        var min = array[i];
+        for(var j = (i+1); j < (array.length-1); j++) {
+          if ( array[j] < array[min]) {
+            min = array[j];
+          }
+        }
+
+        if(array[min] !== j) {
+          var bigger = array[j];
+          var smaller = array[min];
+
+          array[j] = smaller;
+          array[min] = bigger;
+        }
+      }
+      return array;
     }
   };
-});
+})();
