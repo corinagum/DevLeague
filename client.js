e0d32d4759f0c46cb573eac59554a95ca6886439
Corina <corina.aj@gmail.com>
2016-01-05 10:12:18 -1000

    client.js now accepts both localhost and external uri. set default port to 80.

--- client.js
+++ client.js
@@ -1,24 +1,30 @@
 var net = require('net');
 
-var PORT = 8080;
-// var HOST = process.argv[2];
-var HOST = 'localhost';
-var pathName = process.argv[3];
+var PORT = 80;
+var uri = process.argv[2];
+  uri = uri.split('/');
+
+var HOST = uri[0] || localhost;
+var pathName = ('/' + uri[1]) || '/';
 var fileType = pathName.split('.');
     fileType = fileType[fileType.length-1];
-
-var client = net.connect({port:PORT}, function() {
+    console.log(uri, fileType);
+var client = net.connect({
+    port:PORT,
+    host: HOST},
+    function() {
+  // console.log(uri, HOST, pathName);
   client.write("GET " + pathName + " HTTP/1.1\n");
   client.write("Host: " + HOST + "\n");
   client.write("Connection: keep-alive\n");
   client.write("Content-Type : text/" + fileType);
   client.write("Accept: text/html, application/json\n\n");
   // client.write('world!\r\n');
+  client.on('data', function(data) {
+    console.log(data.toString());
+  });
 });
 
-client.on('data', function(data) {
-  console.log(data.toString());
-});
 client.on('end', function() {
   client.end();
 });
