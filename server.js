65a0ac4390c80186507a2d2bc0380e5e1f275682
Corina <corina.aj@gmail.com>
2016-01-06 19:43:42 -1000

    GET request set up to write data on end. POST does not work

--- server.js
+++ server.js
@@ -1,4 +1,57 @@
 var http = require('http');
 var fs = require('fs');
+var querystring = require('querystring');
+var server = http.createServer(onRequest);
 
-var server = http.createServer(onConnect);
+function onRequest(request, response) {
+  var method = request.method;
+  var uri = request.url;
+  var body = "";
+  var parsedChunk;
+  var newElement;
+
+  if(uri === "/") {
+    uri = '/index.html';
+  }
+  //START GET REQUEST
+  if(method === "GET") {
+    response.writeHead(200, {
+      'Content-Type' : 'text/html'
+    });
+    fs.readFile('./public' + uri, function(err, data) {
+      if(err) console.log(err);
+      response.end(data.toString());
+    });
+  }
+  // START POST REQUEST
+  if(method === "POST") {
+
+    request.on('data', function(chunk) {
+      parsedChunk = querystring.parse(chunk.toString());
+    });
+    fs.readFile('./public/template.html', function(err, data) {
+      if(err) console.log(err);
+      newElement = data.toString();
+
+      for (var key in parsedChunk) {
+        newElement = newElement.replace(key, parsedChunk[key]);
+      }
+      console.log(parsedChunk);
+    });
+    response.end();
+  }
+    // console.log(requestHeader);
+
+
+    request.on('end', function() {
+      console.log('end of request');
+    });
+}
+
+server.listen({
+  host : 'localhost',
+  port : 8080
+}, function() {
+  address = server.address();
+  console.log("opened server address: ");
+});
