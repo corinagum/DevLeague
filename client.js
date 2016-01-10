e782d5d79f48b4d7e059f08de588c1e5583127aa
Corina <corina.aj@gmail.com>
2016-01-09 17:59:20 -1000

    adds functionality where server communicates to clients when a new client has signed on or signed off

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
-var Net = require('net');
-var Server = Net.connect({host: 'localhost', port: 6969}, function() {
+var net = require('net');
+
+var Server = net.connect({host: 'localhost', port: 6969}, function() {
   process.stdin.setEncoding('utf-8');
   Server.setEncoding('utf-8');
 
-  var user = "username";
+  var user = "disuser";
+
   process.stdout.write(user + ": ");
   process.stdin.on('data', function(data) {
     Server.write(data);
--- server.js
+++ server.js
@@ -1,21 +1,38 @@
-var Net = require('net');
-var Server = Net.createServer(function(client) {
-  client.write("Welcome to Network Broadcast News");
+var net = require('net');
+var clientList = [];
+
+var Server = net.createServer(function(clientSocket) {
+
+  clientSocket.name = clientSocket.remoteAddress + ":" + clientSocket.remotePort;
+  clientList.push(clientSocket);
+  clientSocket.write("Welcome to Network Broadcast News, " + clientSocket.name);
+  broadcast(clientSocket.name + " joined chat\n", clientSocket);
+
   var body = ''; // do I need this really?
-  client.setEncoding('utf-8');
-
-  client.on('data', function(data){
+  clientSocket.setEncoding('utf-8');
+  clientSocket.on('data', function(data){
     body += data;
-    process.stdout.write("client wrote: " + data);
+    broadcast(clientSocket.name + " wrote: " + data);
+    // clientSocket.write(clientSocket.name + " wrote: " + data);
 
   });
-  client.on('end', function() {
-      var data = JSON.parse(body);
-      client.write(data);
+  clientSocket.on('end', function() {
+    clientList.splice(clientList.indexOf(clientSocket), 1);
+    broadcast(clientSocket.name + " left the chat.\n");
 
   });
+
+  function broadcast(msg, clientSender) {
+    clientList.forEach(function (clientSocket) {
+      if (clientSocket === clientSender) return;
+      // clientSocket.write(msg);
+      // broadcast(clientSocket.name + ": " + msg);
+    });
+
+    process.stdout.write(msg);
+  }
 });
 
 Server.listen(6969, function() {
-  console.log("Server is online");
+  console.log("Chatroom Server is online");
 });
