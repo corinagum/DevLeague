247a1c0c7afc07ca6267712d57e0ebc5ff55c75d
Corina <corina.aj@gmail.com>
2016-01-13 16:34:18 -1000

    initializes body-parser module for use in POST

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var server = express();
 
@@ -5,9 +6,21 @@ var individualBuzzWords = ["what", 'whatwhatwhat'];
 
 server.use(express.static('public'));
 
+// accept GET requests on buzzword page
 server.get('/buzzwords', function (req, res) {
   res.send(individualBuzzWords);
 });
+
+// accept POST request on buzzword page
+server.post('/buzzwords', function(req, res) {
+  var request = req.body;
+  if(request.buzzWord) {
+    res.send( {
+      'success': true
+    });
+  }
+});
+
 
 var app = server.listen(3000, function () {
   var host = app.address().address;
