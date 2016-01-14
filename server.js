9c984943a82e1a00777620a433d4954f0e044d8c
Corina <corina.aj@gmail.com>
2016-01-13 16:14:58 -1000

    establishes GET route handlers for index and buzzwords

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var server = express();
 
+var individualBuzzWords = ["what", 'whatwhatwhat'];
+
 server.use(express.static('public'));
 
+server.get('/buzzwords', function (req, res) {
+  res.send(individualBuzzWords);
+});
 
 var app = server.listen(3000, function () {
   var host = app.address().address;
