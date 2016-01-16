9b6c73874d8f3854c9a44bef65caed9eca3433c1
Corina <corina.aj@gmail.com>
2016-01-16 11:15:03 -1000

    establishes routes for articles

--- routes/articles.js
+++ routes/articles.js
@@ -12,3 +12,42 @@
 
 // //find article in collection by title - update article based on object ... etc.
 // Articles.editByTitle('TITLE', {title: "blahblah"});
+
+
+var Articles = require('./../db/articles.js');
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json({type: 'application/json' }));
+
+
+router.post( '/', function (req, res) {
+  Articles.add(req.body, function(err) {
+    if(err) return res.send( {success: false, message: err.message} );
+
+    return res.send( {success: true} );
+  });
+});
+
+router.put( '/:title', function (req, res) {
+  Articles.editByTitle( req.params.title, req.body, function(err) {
+    if(err) return res.send({success: false, message: err.message});
+
+    return res.send({success: true});
+
+  });
+});
+
+router.delete( '/:title', function (req, res) {
+  Articles.deleteByTitle( req.params.title, function(err) {
+    if(err) return res.send({success: false, message: err.message});
+
+    return res.send({success: true});
+
+  });
+});
+
+
+
+module.exports = router;
