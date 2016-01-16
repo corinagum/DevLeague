e5a351b0b75b84b78f3116c540aafb42e90b547e
Corina <corina.aj@gmail.com>
2016-01-16 11:34:16 -1000

    adds articles routing

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var server = express();
 var products = require('./routes/products.js');
-// var articles = require('./routes/articles.js');
+var articles = require('./routes/articles.js');
 
 server.set('view engine', 'jade');
 server.set('views', './templates/');
 
 server.use('/products', products);
-// server.use('/articles', articles);
+server.use('/articles', articles);
 
 var app = server.listen(3000, function(){
   var host = app.address().address;
