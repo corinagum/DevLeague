d03c0b3449788dc0e3245c26d7ef12ec4e50529c
Corina <corina.aj@gmail.com>
2016-01-22 13:40:50 -1000

    finishes Queries across multiple tables up through 6

--- joins.sql
+++ joins.sql
@@ -15,8 +15,26 @@
 -- WHERE posts.user_id = 200;
 
 -- 4
-SELECT *, users.first_name, users.last_name
+-- SELECT posts.title, users.first_name, users.last_name
+-- FROM posts
+-- INNER JOIN users ON posts.user_id = users.id
+-- WHERE users.first_name = 'Norene" AND
+--       users.last_name = 'Schmitt';
+
+-- 5
+-- SELECT username
+-- FROM users
+-- INNER JOIN posts ON posts.user_id = users.id
+-- WHERE posts.created_at >= '2015-01-01'
+
+-- 6
+-- SELECT title, content, users.username
+-- FROM posts
+-- INNER JOIN users ON posts.user_id = users.id
+-- WHERE users.created_at >= '2015-01-01'
+
+-- 7
+SELECT title, content, users.username
 FROM posts
 INNER JOIN users ON posts.user_id = users.id
-WHERE users.first_name = 'Norene" AND
-      users.last_name = 'Schmitt';
+WHERE users.created_at >= '2015-01-01'
