69a907c08e5c04ee4272af0f5c4e32fab673673b
Corina <corina.aj@gmail.com>
2016-01-22 11:58:06 -1000

    finishes Queries across multiple tables up through 4

--- joins.sql
+++ joins.sql
@@ -1,2 +1,22 @@
-SELECT *
-FROM users
+-- QUERIES ACROSS MULTIPLE TABLES
+-- -- 1
+-- SELECT *
+-- FROM users
+
+-- 2
+-- SELECT *
+-- FROM posts
+-- WHERE posts.user_id = 100;
+
+--3
+-- SELECT *
+-- FROM posts
+-- INNER JOIN users ON posts.user_id = users.id
+-- WHERE posts.user_id = 200;
+
+-- 4
+SELECT *, users.first_name, users.last_name
+FROM posts
+INNER JOIN users ON posts.user_id = users.id
+WHERE users.first_name = 'Norene" AND
+      users.last_name = 'Schmitt';
