b171ffa92b742b91d7e261bd2b2a2f81d719ab69
Corina <corina.aj@gmail.com>
2016-01-25 16:31:56 -1000

    creates three tables products, articles, and authors

--- db/db.sql
+++ db/db.sql
@@ -5,6 +5,8 @@ CREATE USER corizzy_user;
 DROP DATABASE IF EXISTS corizzy_database;
 CREATE DATABASE corizzy_database;
 \c corizzy_database;
+
+-- PRODUCTS TABLE
 DROP TABLE IF EXISTS products_table;
 CREATE TABLE products_table(
   "id" serial NOT NULL,
@@ -12,4 +14,22 @@ CREATE TABLE products_table(
   inventory integer NOT NULL,
   product_name varchar(250) NOT NULL,
   price int NOT NULL DEFAULT 0.0
+  );
+
+-- ARTICLES TABLE
+DROP TABLE IF EXISTS articles_table;
+CREATE TABLE articles_table(
+  "id" serial NOT NULL,
+  PRIMARY KEY("id"),
+  title varchar(250) NOT NULL,
+  urlTitle varchar(450)
+  fk_author_id int REFERENCES authors(id)
+  );
+
+-- AUTHORS TABLE
+DROP TABLE IF EXISTS authors_table;
+CREATE TABLE authors_table(
+  "id" serial NOT NULL,
+  PRIMARY KEY("id"),
+  author varchar(250) NOT NULL,
   );
